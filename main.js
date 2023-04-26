4b599dbe192098be63dbd21aa3d99de6c491e6c6
Dawn Wu <njwzh_dawn@163.com>
2023-04-26 13:46:01 +0800

    temp-4-26

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function createWindow() {
     height: 600,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false,
+      conconfigStringIsolation: false,
       preload: path.join(__dirname, 'preload.js')
     }
   })
@@ -86,26 +86,141 @@ function execCommend(commend){
   });
   return runExec_start
 }
+//parser 重写
+function parseConfig(configString) {
+  configString = String(configString)
+  function commentClear(str){
+    return str.replace(/#.+/g,"")
+  }
+  const configStringWithoutComment = commentClear(configString);
+  const lines = configStringWithoutComment.split('\n');
+  return lines
+
+  let result = {};
+  let currentObject = result;
+  let currentKey = null;
+  
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i].trim();
+
+    // Skip empty lines and comments
+    if (line === "" || line.startsWith("#")) {
+      continue;
+    }
+
+    // Check for opening and closing braces
+    if (line.endsWith("{")) {
+      const key = line.slice(0, -1).trim();
+      currentKey = key;
+      currentObject[key] = {};
+      currentObject = currentObject[key];
+    } else if (line === "}") {
+      currentObject = result;
+      currentKey = null;
+    } else {
+      // Parse key-value pairs
+      const [key, value] = line.split("=");
+      if (!key || !value) {
+        throw new SyntaxError(`Invalid key-value pair: ${line}`);
+      }
+
+      // Trim whitespace from keys and values
+      const trimmedKey = key.trim();
+      const trimmedValue = value.trim();
+
+      // Parse values as booleans or numbers if possible
+      let parsedValue = trimmedValue;
+      if (trimmedValue === "true") {
+        parsedValue = true;
+      } else if (trimmedValue === "false") {
+        parsedValue = false;
+      } else if (!isNaN(trimmedValue)) {
+        parsedValue = parseFloat(trimmedValue);
+      }
+
+      // Set the key-value pair on the current object
+      if (currentKey === null) {
+        throw new SyntaxError(`No object to set key-value pair: ${line}`);
+      }
+      currentObject[trimmedKey] = parsedValue;
+    }
+  }
+
+  return result;
+}
+
+//正则识别IP（配置修改方式一）
+function regLocalIp(string){
+    return string.match(/(?<=local_addrs\D+)((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}/g);
+}
+function regRemoteIp(string){
+    return string.match(/(?<=remote_addrs\D+)((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}/g);
+}
+//json解析（配置修改方式二）
+function jsonModify(config){
+  config.replace(/#.+/g,"")
+}
 //保存配置信息至本地
-ipcMain.on('save_config', async (e, msg) => {
-  console.log('main: ' + msg.localID);
-
-
-});
-
-//文件读写（测试）
-ipcMain.on('read-file-msg', function(event, arg) {
-  // arg是从渲染进程返回来的数据
-  console.log(arg);
-  // 这里是传给渲染进程的数据
+ipcMain.on('save_config', async (event, msg) => {
+  console.log('[save_config] main: ' + msg.localID);
   fs.readFile(path.join(__dirname,"./test.txt"),"utf8",(err,data)=>{
-  if(err){
-		event.sender.send('read-file-reply', "Failure occurs when reading file");
-	}else{
-		event.sender.send('read-file-reply', data);
-	}
+    if(err){
+      event.sender.send('read-file-reply', "Failure occurs when reading file");
+    }else{
+      event.sender.send('read-file-reply', data);
+    }
+    //测试
+    function jsonModify(str){
+      function commentClear(str){
+        return str.replace(/#.+/g,"")
+      }
+      function addComma(data){
+        var passage = data.split("\n")
+        let words_addcomma = [];
+        for (var words of passage){
+          let flag = 0;
+          for(var letter of words){
+            if(letter == "{"){flag = 1}
+          }
+          if (flag == 0){
+            words = words.concat(words + ",");
+            console.log(words);
+          }
+          words_addcomma.push(words);
+        }
+        return words_addcomma.join('\n')
+      }
+      const commentless = commentClear(str)
+      const withquotas = commentless.replace(/(\w|\.|-|\/)(\w|\.|-|\/|^=|\x20)*(\w|\.|-|\/)|\d/g, "\""+"$&"+"\"")
+      const withcolon = withquotas.replace(/{/g, ":"+"$&").replace(/=/g, ":")
+      const withcomma = addComma(withcolon)
+      return "{" + withcomma + "}"
+      // return "{" + withcomma + "}"
+    }
+    // console.log("jsonmodified:\n", jsonModify(data))
+
+    // modified_data = jsonModify(data)
+    // const json_config = JSON.parse(modified_data)
+    const json_config = parseConfig(data);
+    console.log('PARSE DONE: ', json_config)
+    var a = json_config.connection.h2h.local;
+    console.log("local info:",a)
   })
 });
+
+// //文件读写（测试）
+// ipcMain.on('read-file-msg', function(event, arg) {
+//   // arg是从渲染进程返回来的数据
+//   console.log(arg);
+//   fs.readFile(path.join(__dirname,"./test.txt"),"utf8",(err,data)=>{
+//   if(err){
+// 		event.sender.send('read-file-reply', "Failure occurs when reading file");
+// 	}else{
+// 		event.sender.send('read-file-reply', data);
+// 	}
+
+//   })
+// });
 
 //启动StrongSwan
 ipcMain.on('startStrongSwan', async (event, startTime) => {
---
+++ parser.js
@@ -0,0 +1,3 @@
+export function testChildren(a){
+    return a;
+}
